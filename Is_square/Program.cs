dff1336f1d0d68a646d129a8192dd5fcd6207a43
AlexMerk83 <a.merkushenkov@gmail.com>
2023-02-02 12:27:12 +0200

    Init commit Is_square

---
+++ Is_square/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine("Enter two numbers");
+
+if (double.TryParse(Console.ReadLine(), out double a) & double.TryParse(Console.ReadLine(), out double b))
+{
+    if (Math.Pow(b, 2) == a)
+    {
+        System.Console.WriteLine("Yes");
+    }
+    else
+    {
+        System.Console.WriteLine("No");
+    }
+}
+else
+{
+    System.Console.WriteLine("These are not numbers");
+}
+
+System.Console.WriteLine();
