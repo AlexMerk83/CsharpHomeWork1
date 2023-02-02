35cec10b3e613c1a59665fc8ef6e9a006685f1ec
AlexMerk83 <a.merkushenkov@gmail.com>
2023-02-02 18:04:14 +0200

    My version

--- Is_square/Program.cs
+++ Is_square/Program.cs
@@ -1,19 +1,54 @@
-﻿System.Console.WriteLine("Enter two numbers");
+﻿System.Console.WriteLine("Choose the task:");
+System.Console.WriteLine("1 - Is square");
+System.Console.WriteLine("2 - Day of the week");
 
-if (double.TryParse(Console.ReadLine(), out double a) & double.TryParse(Console.ReadLine(), out double b))
+if (int.TryParse(Console.ReadLine(), out int taskNo))
 {
-    if (Math.Pow(b, 2) == a)
+    if (taskNo == 1)
     {
-        System.Console.WriteLine("Yes");
+        System.Console.WriteLine("Enter two numbers");
+
+        if (double.TryParse(Console.ReadLine(), out double a) & double.TryParse(Console.ReadLine(), out double b))
+        {
+            if (Math.Pow(b, 2) == a)
+            {
+                System.Console.WriteLine("Yes");
+            }
+            else
+            {
+                System.Console.WriteLine("No");
+            }
+        }
+        else
+        {
+            System.Console.WriteLine("These are not numbers");
+        }
+    }
+    else if (taskNo == 2)
+    {
+        System.Console.WriteLine("Enter number of the day 1..7");
+        if (int.TryParse(Console.ReadLine(), out int dayNo))
+        {
+            if (dayNo == 1) System.Console.WriteLine("Monday");
+            if (dayNo == 2) System.Console.WriteLine("Tuesday");
+            if (dayNo == 3) System.Console.WriteLine("Wednesday");
+            if (dayNo == 4) System.Console.WriteLine("Thirsday");
+            if (dayNo == 5) System.Console.WriteLine("Friday");
+            if (dayNo == 6) System.Console.WriteLine("Satturday");
+            if (dayNo == 7) System.Console.WriteLine("Sunday");
+            if (dayNo < 1 || dayNo > 7) System.Console.WriteLine("There is no such day of the week");
+        }
+        else
+        {
+            System.Console.WriteLine("There is no such day of the week");  
+        }
     }
     else
     {
-        System.Console.WriteLine("No");
+        System.Console.WriteLine("There is no such option");
     }
 }
 else
 {
-    System.Console.WriteLine("These are not numbers");
+    System.Console.WriteLine("There is no such option");   
 }
-
-System.Console.WriteLine();
